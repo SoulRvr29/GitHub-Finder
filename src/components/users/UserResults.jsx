1fd41f9fa7df9128fe8fe3476919bae7d81b849a
Paweł Chudecki <pawelc29@interia.pl>
2025-02-20 08:52:33 +0100

    GithubContext import fix

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from "react";
-import GithubContext from "../context/github/githubContext";
+import GithubContext from "../context/github/GithubContext";
 import UserItem from "./UserItem";
 
 function UserResults() {
